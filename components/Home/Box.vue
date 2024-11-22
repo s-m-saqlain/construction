001c32cae43e963ad25112e52380965cf8cea316
Saqlain <mohd.313saqlain@gmail.com>
2024-11-22 16:42:05 +0500

    first page

--- components/Home/Box.vue
+++ components/Home/Box.vue
@@ -4,24 +4,24 @@
       <div
         v-for="feature in features"
         :key="feature.title"
-        class="p-6 border border-[#162855] rounded-lg hover:shadow-lg hover:bg-[#162855] group group transition-shadow duration-300 flex flex-col items-center text-center"
+        class="p-6 border border-[#162855] rounded-[50px] hover:shadow-2xl hover:bg-[#162855] group transition-shadow duration-300 flex flex-col items-center text-center"
       >
         <!-- <component :is="feature.icon" class="w-12 h-12 text-sky-400 mb-4" /> -->
         <h3
-          class="text-gray-600 group-hover:text-gray-400 sm:text-2xl font-medium lg:pt-24 sm:pt-4"
+          class="text-gray-600 group-hover:text-gray-400 sm:text-2xl sm:font-bold font-medium lg:pt-24 pt-24"
         >
           {{ feature.sub }}
         </h3>
         <h3
-          class="text-[#162855] group-hover:text-white sm:text-5xl text-4xl lg:py-8 py-4 font-bold"
+          class="text-[#162855] group-hover:text-white sm:text-5xl text-4xl lg:py-8 py-4 sm:font-extrabold font-bold"
         >
           {{ feature.title }}
         </h3>
-        <h3 class="text-gray-500 sm:text-xl group-hover:text-gray-400">
+        <h3 class="text-gray-500 sm:text-xl group-hover:text-gray-400 pb-24 sm:font-bold">
           {{ feature.des }}
         </h3>
 
-        <div class="lg:mt-24 sm:mt-8">
+        <!-- <div class="lg:mt-24 sm:mt-8">
           <div class="sm:flex justify-center item-center gap-6">
             <NuxtLink to="/portfolio">
               <button
@@ -38,7 +38,7 @@
               </button>
             </NuxtLink>
           </div>
-        </div>
+        </div> -->
       </div>
     </div>
   </div>
@@ -55,20 +55,20 @@ interface Feature {
 const features: Feature[] = [
   {
     sub: "Welcome To Mariyah Developers Builde",
-    title: "Constructors And Design Services",
-    des: "Providing Constructors And Design Services",
+    title: "Mariyah Developers Real Estate",
+    des: "Providing Real Estate services",
     // icon: Umbrella,
   },
   {
-    sub: "Best Civil Verse",
-    title: "Civil Verse Design Services",
-    des: "Provide Civil Verse Services",
+    sub: "Best water proofing and other chemical coatings",
+    title: "Water Proofing And Other Chemical Coatings",
+    des: "Provide water proofing and other chemical coatings services",
     // icon: WrenchScrewdriver,
   },
   {
-    sub: "Best Home Renovation",
-    title: "Home Renovation Design Services",
-    des: "Provide Home Renovation Services",
+    sub: "Best civil contractors and home renovations",
+    title: "Civil Contractors And Home Renovations",
+    des: "Provide civil contractors and home renovations services",
     // icon: Tag,
   },
 ];
